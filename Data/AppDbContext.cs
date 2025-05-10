66964b854c7aba8be25d5dcc21cda596b7a43c93
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2025-05-09 22:33:11 -0300

    Criação da tabela Livros

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Biblioteca.Data
 {
@@ -7,5 +8,7 @@ namespace Biblioteca.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<LivrosModel> Livros { get; set; }
     }
 }
---
+++ Models/LivrosModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Models
+{
+    public class LivrosModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; } = string.Empty;
+        [Required]
+        public string Autor { get; set; } = string.Empty;
+        [Required]
+        public string Descricao { get; set; } = string.Empty;
+        [Required]
+        public string Capa { get; set; } = string.Empty;
+        [Required]
+        public string ISBN { get; set; } = string.Empty;
+        [Required]
+        public string Genero { get; set; } = string.Empty;
+        [Required]
+        public int AnoPublicacao { get; set; }
+        [Required]
+        public int QuantidadeEstoque { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+        public DateTime DataAlteracao { get; set; } = DateTime.Now;
+    }
+}
